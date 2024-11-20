b3c25db764ce19bc4a5b8ef233dabcbd33fea4e4
matos <matosyanalex04@gmail.com>
2024-11-20 22:35:56 +0300

    fixes in metrics

--- src/metrics/utils.py
+++ src/metrics/utils.py
@@ -1,7 +1,24 @@
-from torchmetrics.audio import SignalDistortionRatio
+from torchmetrics.audio import ScaleInvariantSignalDistortionRatio, ScaleInvariantSignalNoiseRatio
+
+
+def calc_si_sdr(predicted, target):
+    si_sdr = ScaleInvariantSignalDistortionRatio().to('cuda')
+    
+    return si_sdr(predicted, target)
 
 
 def calc_si_sdri(predicted, target, mix):
-    si_sdr = SignalDistortionRatio().to('cuda')
+    si_sdr = ScaleInvariantSignalDistortionRatio().to('cuda')
 
     return si_sdr(predicted, target) - si_sdr(mix, target)
+
+def calc_si_snr(predicted, target):
+    si_snr = ScaleInvariantSignalNoiseRatio().to('cuda')
+    
+    return si_snr(predicted, target)
+
+
+def calc_si_snri(predicted, target, mix):
+    si_snr = ScaleInvariantSignalNoiseRatio().to('cuda')
+
+    return si_snr(predicted, target) - si_snr(mix, target)
